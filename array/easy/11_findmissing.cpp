5c225be0bb23d9f0a1b35af38ce6a7a350c57f87
devdutt-pedalsup <devdutt@pedalsup.com>
2022-12-16 19:02:39 +0530

    mids

--- array/easy/11_findmissing.cpp
+++ array/easy/11_findmissing.cpp
@@ -6,15 +6,25 @@ using namespace std;
 pair<int, int> missing(vector<int>& arr){
   pair<int, int> p;
   int len = arr.size();
+  // expected
   int sum = (len*(len+1))/2;
+  int sumOfSquare = (len*(len+1)*(2*len+1))/6;
 
+  for( auto i:arr ){
+    sum -= i;
+    sumOfSquare -= (i*i);
+  }
 
+  int missing = (sum + (sumOfSquare/sum))/2;
+
+  cout << "a: " << missing << endl;
+  cout << "b: " << sumOfSquare << endl;
 
   return p;
 }
 
 int main() {
-  vector<int> arr = { 1, 2, 3, 3, 5, 6 };
+  vector<int> arr = { 1, 2, 2, 4, 5, 6 };
 
   missing(arr);
 
