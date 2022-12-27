4bfe0bf99223d2215f3ca5ca7b3fc89e4598dbbf
devdutt-pedalsup <devdutt@pedalsup.com>
2022-12-27 18:08:59 +0530

    prepare for next

---
+++ BS/ss/6_shippackages.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Q. Given an array arr[] of N weights. Find the least weight capacity of a boat to ship all weights within D days.The ith weight has a weight of arr[i]. Each day, we load the boat with weights given by arr[i].We may not load more weight than the maximum weight capacity of the ship.
+
+int main(){
+
+  return 0;
+}
